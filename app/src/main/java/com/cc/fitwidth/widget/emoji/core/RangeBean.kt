3e6969f1e6e76f6b4b26a134ba6fb59965266399
khaos116 <khaos116@outlook.com>
2021-08-09 19:48:11 +0700

    clickSpan

--- app/src/main/java/com/cc/fitwidth/widget/emoji/core/RangeBean.kt
+++ app/src/main/java/com/cc/fitwidth/widget/emoji/core/RangeBean.kt
@@ -1,5 +1,7 @@
 package com.cc.fitwidth.widget.emoji.core
 
+import android.text.style.ClickableSpan
+
 /**
  * @Description
  * @Author：Khaos
@@ -14,7 +16,9 @@ data class RangeBean(
 data class Range(
   var start: Int = 0, //整行文字的开始位置
   var end: Int = 0, //整行文字的结束位置
-  val type: Int = 0, //0普通;1背景色;2前景色;3背景和前景色
+  val type: Int = 0, //0普通;1背景色;2前景色;3背景和前景色;4clickSpan
   val bgColor: Int = 0, //背景色
   val foreColor: Int = 0, //前景色
+  val split: Boolean = false, //是否被分隔成了多行
+  var clickSpan: ClickableSpan? = null, //带点击的span
 )
